98fcdb78f62afbc1118ed801663c44443cd15384
Eugene Orlov <unimws@gmail.com>
2023-02-06 02:00:39 +0300

    initial commit: added .gitignore and SalesManager class

---
+++ src/SalesManager.java
@@ -0,0 +1,17 @@
+public class SalesManager {
+    protected int[] sales;
+
+    public SalesManager(int[] sales) {
+        this.sales = sales;
+    }
+
+    public int max() {
+        int max = -1;
+        for (int sale : sales) {
+            if (sale > max) {
+                max = sale;
+            }
+        }
+        return max;
+    }
+}
